3541933be55729e196f2f77418af54ea01996a62
felipekian <felipekian3@gmail.com>
2022-12-07 15:14:36 -0400

    articles controle

---
+++ scripts/articles/form_control.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+function category() {
+  let select_categories = document.querySelector('#jform_catid');
+  let select_fake = document.querySelector('#jform_catid_chzn > a > span');
+
+  let OPTION = "Notícias";
+
+  // select_categories.style.display = "block";
+
+  let options_categories = [...select_categories.options];
+
+  for (let i = 0; i < options_categories.length; i++) {
+    if(options_categories[i].innerText == OPTION){
+      options_categories[i].selected = true;
+      break;
+    }    
+  }
+
+  select_fake.innerHTML = OPTION;
+  select_fake.classList.add('result-selected');
+  select_fake.classList.add('highlighted');
+}
+
+
+function start() {
+  category();
+}
+
+start();
