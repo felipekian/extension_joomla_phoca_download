c7e8079ae483b756f83c187a54f350275a7830a7
felipekian <felipekian3@gmail.com>
2023-09-21 14:27:13 -0400

    add date sempre a meia noite

--- scripts/phoca/date_start_publish_controler.js
+++ scripts/phoca/date_start_publish_controler.js
@@ -3,9 +3,9 @@
 function setDateStartPublishInput(data_publish, date_old_publish) {
   if (date_old_publish === getDateNow()) {
     let input_date_publish = document.querySelector('#jform_publish_up');
-    input_date_publish.dataset.altValue = ""+data_publish;
-    input_date_publish.dataset.localValue = ""+data_publish;
-    input_date_publish.value = ""+data_publish;
+    input_date_publish.dataset.altValue = "" + data_publish;
+    input_date_publish.dataset.localValue = "" + data_publish;
+    input_date_publish.value = "" + data_publish;
     input_date_publish.click();
   }
 }
@@ -16,11 +16,16 @@ function getDateNow() {
   return today.toDateString(); // "Sun Jan 30 2022"
 }
 
+function format_data_para_amanha(data) {
+  data = data.split(' ');
+  return data[0] + ' 00:00:00';
+}
+
 function startPublishInputControl() {
   setDateStartPublishInput(localStorage.getItem('joomla_start_publish'), localStorage.getItem('joomla_start_publish_date'));
 
   document.querySelector('#jform_publish_up').addEventListener('blur', (event) => {
-    const data = event.target.value;
+    let data = format_data_para_amanha(event.target.value);
     localStorage.setItem('joomla_start_publish', data);
     localStorage.setItem('joomla_start_publish_date', getDateNow());
     setDateStartPublishInput(data, localStorage.getItem('joomla_start_publish_date'));
