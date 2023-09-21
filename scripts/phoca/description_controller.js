75461fee841f56221170a040dbf57e07feb39c1f
felipekian <felipekian3@gmail.com>
2023-09-21 15:18:29 -0400

    add data para controle de limpeza da descricao

--- scripts/phoca/description_controller.js
+++ scripts/phoca/description_controller.js
@@ -1,22 +1,43 @@
 "use strict"
 
+
+/***********************************************
+  Verifica a data atual e a salva no localstorage são diferentes para limpar a descrição
+***********************************************/
+
+if (getDateNow() != localStorage.getItem('textarea_description_date')) {
+  remover_descricao();
+}
+
+
+/***********************************************
+  Adicionar descrição
+***********************************************/
 function adicionar_descricao() {
   let descricao = window.prompt('Descrição:');
 
   if (!descricao) return;
 
   localStorage.setItem('textarea_description', descricao);
+  localStorage.setItem('textarea_description_date', getDateNow());
 
   location.reload();
 }
 
+
+/***********************************************
+  Remover descrição
+***********************************************/
 function remover_descricao() {
   localStorage.setItem('textarea_description', '');
-  
+
   location.reload();
 }
 
 
+/***********************************************
+  Configuração de atalhos
+***********************************************/
 document.addEventListener("keydown", function (event) {
   /* 
     Adicionar descricao
@@ -39,14 +60,16 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-/* 
+
+
+/************************************************ 
   Popular descricao na página
-*/
+************************************************/
 function setDescriptionAutomaticIfExists() {
 
-  if (!localStorage.getItem('textarea_description')) return;
-  if (localStorage.getItem('textarea_description') == '') return;
-  if (localStorage.getItem('textarea_description').length < 1) return;
+  if (!localStorage.getItem('textarea_description') || localStorage.getItem('textarea_description').length < 1) {
+    adicionar_descricao();
+  }
 
   document.querySelector('#jform_description').innerHTML = localStorage.getItem('textarea_description');
 
@@ -54,18 +77,18 @@ function setDescriptionAutomaticIfExists() {
 
 setDescriptionAutomaticIfExists();
 
-/* 
-  Botoes de controle das funções
-*/
+
+
+/********************************************** 
+  Botoes de controle das funções de adicionar e remover descrição
+**********************************************/
 let botao_descricao_adicionar = document.createElement('button');
 botao_descricao_adicionar.innerHTML = 'Adicionar';
 botao_descricao_adicionar.title = "Adicionar descrição automatica (Ctrl + Alt + D)";
 botao_descricao_adicionar.classList.add('btn');
 botao_descricao_adicionar.classList.add('btn-small');
 botao_descricao_adicionar.addEventListener('click', () => {
-
   adicionar_descricao();
-
 });
 
 let botao_descricao_remover = document.createElement('button');
@@ -74,9 +97,7 @@ botao_descricao_remover.classList.add('btn');
 botao_descricao_remover.classList.add('btn-small');
 botao_descricao_remover.title = 'Remover descrição automatica (Ctrl + Alt + R)';
 botao_descricao_remover.addEventListener('click', () => {
-
   remover_descricao();
-
 });
 
 document.querySelector('#toolbar-cancel').append(botao_descricao_adicionar);
