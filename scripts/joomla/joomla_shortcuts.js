56a09280d76e2fcadbb8445ee88c21e53e0ced2b
felipekian <felipekian3@gmail.com>
2023-09-28 08:25:11 -0400

    add shortcut cache e ir para pagina de listagem phoca downloads

---
+++ scripts/joomla/joomla_shortcuts.js
@@ -0,0 +1,18 @@
+"use strict"
+
+document.addEventListener("keydown", function (event) {
+  /************************* 
+    pagina phoca downloads listagem
+  *************************/
+  if (event.ctrlKey && event.shiftKey && (event.key === 'k' || event.key === 'K')) {
+    
+    window.location = 'https://saude.rr.gov.br/administrator/index.php?option=com_phocadownload&view=phocadownloadfiles';
+  }
+
+  /************************* 
+    pagina de cache
+  *************************/
+  if (event.ctrlKey && event.shiftKey && (event.key === 'l' || event.key === 'L')) {
+    window. open("https://saude.rr.gov.br/administrator/index.php?option=com_cache", "_blank");
+  }
+});
