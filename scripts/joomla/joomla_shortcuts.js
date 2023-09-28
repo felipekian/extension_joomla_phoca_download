dc9c4a2ed97831bd660819888b61413d9fe1246a
felipekian <felipekian3@gmail.com>
2023-09-28 08:38:43 -0400

    ajuste

--- scripts/joomla/joomla_shortcuts.js
+++ scripts/joomla/joomla_shortcuts.js
@@ -4,13 +4,12 @@ document.addEventListener("keydown", function (event) {
   /************************* 
     pagina phoca downloads listagem
   *************************/
-  if (event.ctrlKey && event.shiftKey && (event.key === 'k' || event.key === 'K')) {
-    
+  if (event.ctrlKey && event.shiftKey && (event.key === 'k' || event.key === 'K')) {    
     window.location = 'https://saude.rr.gov.br/administrator/index.php?option=com_phocadownload&view=phocadownloadfiles';
   }
 
   /************************* 
-    pagina de cache
+    página de cache
   *************************/
   if (event.ctrlKey && event.shiftKey && (event.key === 'l' || event.key === 'L')) {
     window. open("https://saude.rr.gov.br/administrator/index.php?option=com_cache", "_blank");
