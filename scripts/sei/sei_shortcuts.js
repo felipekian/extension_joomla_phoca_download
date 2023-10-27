84cf3184078bb7b570857b56a6b0458be9c3c2a0
felipekian <felipekian3@gmail.com>
2023-10-27 11:12:19 -0400

    reload sei home

--- scripts/sei/sei_shortcuts.js
+++ scripts/sei/sei_shortcuts.js
@@ -1,12 +1,14 @@
 "use strict"
 
-document.querySelector('#lnkControleProcessos').title="Alt + Q";
+let lnkControleProcessos = document.querySelector('#lnkControleProcessos');
+
+lnkControleProcessos.title = "Alt + Q";
 
 document.addEventListener("keydown", function (event) {
   /************************* 
     clica no botão home
   *************************/
   if (event.altKey && (event.key === 'q' || event.key === 'Q')) {
-    document.querySelector('#lnkControleProcessos').click();
+    lnkControleProcessos.click();
   }
 });
