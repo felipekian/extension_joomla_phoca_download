1a8c546741c99c124d7e09925bfcbc782ed6a7c9
felipekian <felipekian3@gmail.com>
2023-11-13 14:18:01 -0400

    ajuste listagem por ID com valor da option em vez do texto

--- scripts/phoca/select_listagem_by_id_desc.js
+++ scripts/phoca/select_listagem_by_id_desc.js
@@ -5,7 +5,7 @@ function select_listagem_by_id_desc() {
   let select_options_order = document.querySelector('#list_fullordering');
   let select_fake = document.querySelector('#list_fullordering_chzn > a > span');
 
-  let OPTION = "ID descending";
+  let OPTION = "a.id DESC";
 
   // select_options_order.style.display = "block";
 
@@ -17,11 +17,11 @@ function select_listagem_by_id_desc() {
   for (let i = 0; i < options_categories.length; i++) {
     
     /* refresh caso esteja outra option selecionada */
-    if (options_categories[i].selected && options_categories[i].innerText != OPTION) {
+    if (options_categories[i].selected && options_categories[i].value != OPTION) {
       refresh_page = true;
     }
 
-    if (options_categories[i].innerText == OPTION) {
+    if (options_categories[i].value == OPTION) {
       options_categories[i].selected = true;
       break;
     }
