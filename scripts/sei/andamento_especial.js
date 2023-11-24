1eb22544de0fa611af7f6f2b4658e38537b248c1
felipekian <felipekian3@gmail.com>
2023-11-24 16:55:05 -0400

    add shorcut para selecionar CSL andamento especial

---
+++ scripts/sei/andamento_especial.js
@@ -0,0 +1,16 @@
+"use strict"
+
+window.addEventListener("keydown", function (event) {
+  if (event.altKey && (event.key === 'c' || event.key === 'C')) {
+    let select_setor = document.querySelector('#selGrupoAcompanhamento');
+    let option_csl = document.querySelector('#selGrupoAcompanhamento > option:nth-child(17)');
+
+    if (!option_csl.selected) {
+      option_csl.selected = true;
+      select_setor.style.border = '2px solid green';
+    }
+  }
+});
+
+const label_acompanhamento = document.querySelector('#lblSelGrupoAcompanhamento');
+label_acompanhamento.innerHTML = label_acompanhamento.innerHTML + " \"(Alt + C) para selecionar CSL\"";
