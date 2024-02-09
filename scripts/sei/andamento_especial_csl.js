f08ef1308ff79a4f5ccb119c89b6334e8c7c1c30
felipekian <felipekian3@gmail.com>
2024-02-09 10:12:39 -0400

    sei ajustados window load

--- scripts/sei/andamento_especial_csl.js
+++ scripts/sei/andamento_especial_csl.js
@@ -1,24 +1,21 @@
 "use strict"
 
-window.addEventListener('load', () => {
-  
-  window.addEventListener("keydown", function (event) {
-    if (event.altKey && (event.key === 'c' || event.key === 'C')) {
-      let select_setor = document.querySelector('#selGrupoAcompanhamento');
-      let option_csl = document.querySelector('#selGrupoAcompanhamento > option:nth-child(17)');
 
-      if (!option_csl.selected) {
-        option_csl.selected = true;
-        select_setor.style.border = '2px solid green';
-      }
+window.addEventListener("keydown", function (event) {
+  if (event.altKey && (event.key === 'c' || event.key === 'C')) {
+    let select_setor = document.querySelector('#selGrupoAcompanhamento');
+    let option_csl = document.querySelector('#selGrupoAcompanhamento > option:nth-child(17)');
+
+    if (!option_csl.selected) {
+      option_csl.selected = true;
+      select_setor.style.border = '2px solid green';
     }
-  });
+  }
+});
 
-<<<<<<< HEAD
+
 window.addEventListener('load', () => {
 
-=======
->>>>>>> e0ead41d7e1a33bcd4ed1c591dcb13b59a29d44b
   const label_acompanhamento = document.querySelector('#lblSelGrupoAcompanhamento');
   label_acompanhamento.innerHTML = label_acompanhamento.innerHTML + " \"(Alt + C) para selecionar CSL\"";
 
--- scripts/sei/sei_shortcuts.js
+++ scripts/sei/sei_shortcuts.js
@@ -3,7 +3,6 @@
 window.addEventListener('load', () => {
 
   let lnkControleProcessos = document.querySelector('#lnkControleProcessos');
-<<<<<<< HEAD
   lnkControleProcessos.title = "Alt + Q";
 
 });
@@ -19,22 +18,4 @@ window.addEventListener("keydown", function (event) {
   if (event.altKey && (event.key === 's' || event.key === 'S')) {
     document.querySelector('#page_joomla').click();
   }
-=======
-
-  lnkControleProcessos.title = "Alt + Q";
-
-  window.addEventListener("keydown", function (event) {
-    /************************* 
-      clica no botão home
-    *************************/
-    if (event.altKey && (event.key === 'q' || event.key === 'Q')) {
-      lnkControleProcessos.click();
-    }
-
-    if (event.altKey && (event.key === 's' || event.key === 'S')) {
-      document.querySelector('#page_joomla').click();
-    }
-  });
-
->>>>>>> e0ead41d7e1a33bcd4ed1c591dcb13b59a29d44b
 });
