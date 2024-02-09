bac69ddb6b6e2d6b4737117393c708fc43797ac3
felipekian <felipekian3@gmail.com>
2024-02-09 10:33:09 -0400

    date start load

--- scripts/phoca/date_start_publish_controler.js
+++ scripts/phoca/date_start_publish_controler.js
@@ -1,54 +1,44 @@
 "use strict"
 
-window.addEventListener('load', () => {
 
-  function setDateStartPublishInput(data_publish, date_old_publish) {
-    if (date_old_publish === getDateNow()) {
-      let input_date_publish = document.querySelector('#jform_publish_up');
-      input_date_publish.dataset.altValue = "" + data_publish;
-      input_date_publish.dataset.localValue = "" + data_publish;
-      input_date_publish.value = "" + data_publish;
-      input_date_publish.click();
-    }
+function setDateStartPublishInput(data_publish, date_old_publish) {
+  if (date_old_publish === getDateNow()) {
+    let input_date_publish = document.querySelector('#jform_publish_up');
+    input_date_publish.dataset.altValue = "" + data_publish;
+    input_date_publish.dataset.localValue = "" + data_publish;
+    input_date_publish.value = "" + data_publish;
+    input_date_publish.click();
   }
+}
 
-  function getDateNow() {
-    const timeElapsed = Date.now();
-    const today = new Date(timeElapsed);
-    return today.toDateString(); // "Sun Jan 30 2022"
-  }
+function getDateNow() {
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+  return today.toDateString(); // "Sun Jan 30 2022"
+}
 
-<<<<<<< HEAD
 function format_data_para_amanha(data) {
   if (data === '') return data;
   data = data.split(' ');
   return data[0] + ' 00:00:00';
 }
-=======
-  function format_data_para_amanha(data) {
-    if (data === '') return data;
-    data = data.split(' ');
-    return data[0] + ' 00:00:00';
-  }
->>>>>>> e0ead41d7e1a33bcd4ed1c591dcb13b59a29d44b
 
-  function startPublishInputControl() {
-    setDateStartPublishInput(localStorage.getItem('joomla_start_publish'), localStorage.getItem('joomla_start_publish_date'));
+function startPublishInputControl() {
+  setDateStartPublishInput(localStorage.getItem('joomla_start_publish'), localStorage.getItem('joomla_start_publish_date'));
 
-    document.querySelector('#jform_publish_up').addEventListener('blur', (event) => {
-      let data = format_data_para_amanha(event.target.value);
-      localStorage.setItem('joomla_start_publish', data);
-      localStorage.setItem('joomla_start_publish_date', getDateNow());
-      setDateStartPublishInput(data, localStorage.getItem('joomla_start_publish_date'));
-    });
-  }
+  document.querySelector('#jform_publish_up').addEventListener('blur', (event) => {
+    let data = format_data_para_amanha(event.target.value);
+    localStorage.setItem('joomla_start_publish', data);
+    localStorage.setItem('joomla_start_publish_date', getDateNow());
+    setDateStartPublishInput(data, localStorage.getItem('joomla_start_publish_date'));
+  });
+}
 
-<<<<<<< HEAD
 /**
  *
  * Start
  * 
- **/ 
+ **/
 
 window.addEventListener('load', () => {
 
@@ -56,8 +46,3 @@ window.addEventListener('load', () => {
 
 });
 
-=======
-  startPublishInputControl();
-
-});
->>>>>>> e0ead41d7e1a33bcd4ed1c591dcb13b59a29d44b
