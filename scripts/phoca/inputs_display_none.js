a2846f999b9fa2edf5ef932fdb9d62cfc47f90ae
felipekian <felipekian3@gmail.com>
2024-02-09 10:21:15 -0400

    inputs_display_none load

--- scripts/phoca/inputs_display_none.js
+++ scripts/phoca/inputs_display_none.js
@@ -1,24 +1,13 @@
 "use strict"
 
-window.addEventListener('load', () => {
-  
-  function displayNoneIputs() {
 
-    document.querySelector('#general > div:nth-child(2)').style.display = "none";
-    for (let index = 6; index < 26; index++) {
-      document.querySelector(`#general > div:nth-child(${index})`).style.display = "none";
-    }
+function displayNoneIputs() {
 
-    /* Campos de textos */
-    document.querySelector('#general > div:nth-child(31) > div.editor.wf-editor-container.mb-2').style.display = 'none';
-
-    document.querySelector('#general > div:nth-child(34) > div.editor.wf-editor-container.mb-2').style.display = 'none';
-
-    document.querySelector('#general > div:nth-child(37) > div.editor.wf-editor-container.mb-2').style.display = 'none';
-
+  document.querySelector('#general > div:nth-child(2)').style.display = "none";
+  for (let index = 6; index < 26; index++) {
+    document.querySelector(`#general > div:nth-child(${index})`).style.display = "none";
   }
 
-<<<<<<< HEAD
   /* Campos de textos */
   document.querySelector('#general > div:nth-child(31) > div.editor.wf-editor-container.mb-2').style.display = 'none';
 
@@ -28,18 +17,9 @@ window.addEventListener('load', () => {
 
 }
 
-/**
- *
- * Start 
- * 
- **/
 
 window.addEventListener('load', () => {
 
   displayNoneIputs();
 
-=======
-  displayNoneIputs();
-
->>>>>>> e0ead41d7e1a33bcd4ed1c591dcb13b59a29d44b
 });
