ba6f5708aea3f6bb01df2010366a2dac0c3f879e
felipekian <felipekian3@gmail.com>
2026-08-14 11:26:26 -0400

    refactor order list files

--- scripts/phoca/phoca_downloads_listagem_por_id_desc.js
+++ scripts/phoca/phoca_downloads_listagem_por_id_desc.js
@@ -3,29 +3,28 @@
 
 const PhocaDownloadsListagemByIdDesc = {
 
-  handle: function() {
+  handle: function () {
 
     let select_options_order = document.querySelector('#list_fullordering');
-    let select_fake = document.querySelector('#list_fullordering_chzn > a > span');
-  
+
     let OPTION = "a.id DESC";
-  
+
     // select_options_order.style.display = "block";
-  
+
     let options_categories = [...select_options_order.options];
-  
+
     let refresh_page = false;
-  
+
     /* changer selected */
     for (let i = 0; i < options_categories.length; i++) {
-  
+
       /* refresh caso esteja outra option selecionada */
       if (options_categories[i].selected && options_categories[i].value != OPTION) {
 
         refresh_page = true;
-        
+
       }
-  
+
       if (options_categories[i].value == OPTION) {
 
         options_categories[i].selected = true;
@@ -34,8 +33,8 @@ const PhocaDownloadsListagemByIdDesc = {
       }
 
     }
-  
-    if (refresh_page){
+
+    if (refresh_page) {
 
       document.querySelector('#adminForm').submit();
 
